febe4bb8ef71f442fd0053c572c3368a2e8bdcf7
Daniel Amorim <daniel.amorim@idp.lu>
2024-11-20 12:55:04 +0100

    update

---
+++ my-sveltekit-app/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+  <script>
+    export let data;
+    let posts = data.posts.data;
+  
+    $: console.log('data:', data.posts.data);
+  </script>
+  
+  <h1>Posts</h1>
+    <ul>
+      {#each posts as post}
+        <li>
+          <a href={`/posts/${post.id}`}>{post.attributes.Title}</a>
+        </li>
+      {/each}
+    </ul>
+
+  
